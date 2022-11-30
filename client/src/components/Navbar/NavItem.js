4ae84883252724913cfa57e63f9c8b6626480172
Miroslav Balev <m.balev@editcreation.com>
2022-11-30 11:06:43 +0200

    add cart button to header

--- client/src/components/Navbar/NavItem.js
+++ client/src/components/Navbar/NavItem.js
@@ -22,20 +22,23 @@ const NavItem = ({
         <Button
             component={RouterLink}
             sx={{
+                display: 'flex',
+                justifyContent: 'space-evenly',
                 color: rest.textcolor || 'text.black',
                 fontSize: 16,
                 fontWeight: 'medium',
                 letterSpacing: 0,
-                py: 0.75,
+                py: rest.paddingY || 0.75,
+                px: rest.paddingX || 0,
                 mx: 3,
                 textTransform: 'none',
                 width: '100%',
                 ...(active && {
                     color: 'primary.contrastText'
                 }),
-                '& svg': {
-                    mr: 1
-                },
+                // '& svg': {
+                //     mr: 1
+                // },
             }}
             to={href}
             {...rest}
@@ -49,7 +52,6 @@ const NavItem = ({
 
 NavItem.propTypes = {
     href: PropTypes.string,
-    title: PropTypes.string
 };
 
 export default NavItem;
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,37 +1,68 @@
 import { Link as RouterLink } from 'react-router-dom';
-import { AppBar, Toolbar, Box, Button } from '@mui/material';
+import { AppBar, Toolbar, Box, Divider, Badge } from '@mui/material';
 import Logo from '../Logo';
 import NavItem from './NavItem';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { styled } from '@mui/material/styles';
 
-const Navbar = (props) => (
-  <AppBar
-    elevation={0}
-    sx={{ py: 1 }}
-    {...props}
-  >
-    <Toolbar sx={{ height: 64, display: 'flex', justifyContent: 'space-around' }}>
-      <RouterLink to="/">
-        <Logo />
-      </RouterLink>
-      <Box sx={{  display: 'flex', width: '20%', justifyContent: 'space-between'  }}>
-        <NavItem
-          title="Начало"
-          href="/"
-        />
-        <NavItem
-          title="Продукти"
-          href="/products"
-        />
-        <NavItem
-          title="Вход"
-          href="/login"
-          variant="contained"
-          color="bordoRed"
-          textcolor="text.white"
-        />
-      </Box>
-    </Toolbar>
-  </AppBar>
-);
+const Navbar = (props) => {
+    const StyledBadge = styled(Badge)(({ theme }) => ({
+        '& .MuiBadge-badge': {
+            position: 'relative',
+            width: '28px',
+            height: '28px',
+            borderRadius: '28px',
+            right: 6,
+            top: 14,
+            border: `2px solid ${theme.palette.background.paper}`,
+            padding: '0 4px',
+        },
+    }));
+
+    return (
+        <AppBar
+            elevation={0}
+            sx={{ py: 1, px: 55 }}
+            {...props}
+        >
+            <Toolbar sx={{ height: 64, display: 'flex', justifyContent: 'space-between' }}>
+                <RouterLink to="/">
+                    <Logo />
+                </RouterLink>
+                <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <NavItem
+                        title="Начало"
+                        href="/"
+                    />
+                    <NavItem
+                        title="Продукти"
+                        href="/products"
+                    />
+                    <Divider orientation="vertical" flexItem />
+                    <NavItem
+                        title={
+                            <StyledBadge badgeContent={4} color="bordoRed">
+                                <span>Количка</span>
+                            </StyledBadge>
+                        }
+                        href="/cart"
+                        variant="outlined"
+                        color="bordoRed"
+                        textcolor="bordoRed"
+                        paddingX={4.5}
+                        startIcon={<ShoppingCartIcon />}
+                    />
+                    <NavItem
+                        title="Вход"
+                        href="/login"
+                        variant="contained"
+                        color="bordoRed"
+                        textcolor="text.white"
+                    />
+                </Box>
+            </Toolbar>
+        </AppBar>
+    );
+};
 
 export default Navbar;
