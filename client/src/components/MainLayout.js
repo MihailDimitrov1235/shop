f0a379374cc958112a4343c8a810462d3b9e7f15
Miroslav Balev <m.balev@editcreation.com>
2022-12-01 10:06:28 +0200

    main layout wrappers

--- client/src/components/MainLayout.js
+++ client/src/components/MainLayout.js
@@ -1,13 +1,48 @@
 import { Outlet } from 'react-router-dom';
+import { styled } from '@mui/styles';
 import Navbar from './Navbar/Navbar';
 
+const MainLayoutRoot = styled('div')(
+    ({ theme }) => ({
+        backgroundColor: theme.palette.background.default,
+        display: 'flex',
+        height: '100%',
+        overflow: 'hidden',
+        width: '100%'
+    })
+);
+
+const MainLayoutWrapper = styled('div')({
+    display: 'flex',
+    flex: '1 1 auto',
+    overflow: 'hidden',
+    paddingTop: 84
+});
+
+const MainLayoutContainer = styled('div')({
+    display: 'flex',
+    flex: '1 1 auto',
+    overflow: 'hidden'
+});
+
+const MainLayoutContent = styled('div')({
+    flex: '1 1 auto',
+    height: '100%',
+    overflow: 'auto'
+});
+
 const MainLayout = () => {
-    return(
-        <>
+    return (
+        <MainLayoutRoot>
             <Navbar />
-            <Outlet />
-        </>
-        
+            <MainLayoutWrapper>
+                <MainLayoutContainer>
+                    <MainLayoutContent>
+                        <Outlet />
+                    </MainLayoutContent>
+                </MainLayoutContainer>
+            </MainLayoutWrapper>
+        </MainLayoutRoot>
     );
 };
 
