13a331240b954d2d943ff40c5ade5cd97044ce16
Misho <Mighty.strong1235@gmail.com>
2022-12-03 23:55:56 +0200

    Updated sideBar for admin

--- admin/src/pages/global/sideBar.jsx
+++ admin/src/pages/global/sideBar.jsx
@@ -1,32 +1,47 @@
 import { useState } from "react";
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { Sidebar,useProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 //import 'react-pro-sidebar/dist/styles';
 import { Box, useTheme } from "@mui/system";
 import Typography from '@mui/material/Typography';
 import { Link } from "react-router-dom";
-import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
 import { IconButton } from "@mui/material";
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 const SideBar = () =>{
     const theme = useTheme();
+    const { collapseSidebar } = useProSidebar();
 
     return (
         <Box sx={{
+            display:'flex',
             height:'100%',
-            width:'300px',
 
         }}>
-            <Menu>
-                <SubMenu label="People" >
-                    <MenuItem> Users </MenuItem>
-                    <MenuItem> Admins </MenuItem> 
-                </SubMenu>
-                <SubMenu label="Projects">
-                    <MenuItem> Add Project </MenuItem>
-                    <MenuItem> View Projects </MenuItem>
-                </SubMenu>
-                <MenuItem> Calendar </MenuItem>
-            </Menu>
+            <Sidebar>
+                <Menu>
+                    <SubMenu label="Accounts" icon=<ManageAccountsIcon/> >
+                        <MenuItem> Users </MenuItem>
+                        <MenuItem> Admins </MenuItem> 
+                    </SubMenu>
+                    <SubMenu label="Projects" icon=<ShoppingBagIcon/> >
+                        <MenuItem> Add Project </MenuItem>
+                        <MenuItem> View Projects </MenuItem>
+                    </SubMenu>
+                    <MenuItem icon=<CalendarMonthIcon/> > Calendar </MenuItem>
+                </Menu>
+            </Sidebar>
+
+            <main>
+                <button onClick={() => collapseSidebar()}>Collapse</button>
+            </main>
         </Box>
     )
 }
