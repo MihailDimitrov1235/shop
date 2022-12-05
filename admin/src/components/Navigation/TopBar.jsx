c91d4d1b5011d429949c9b03f5bef3dbcc48d1e0
MihailDimitrov1235 <107298750+MihailDimitrov1235@users.noreply.github.com>
2022-12-05 18:55:46 +0200

    Update TopBar.jsx

--- admin/src/components/Navigation/TopBar.jsx
+++ admin/src/components/Navigation/TopBar.jsx
@@ -1,56 +1,49 @@
-import {Box, IconButton} from "@mui/material";
-import { useTheme } from '@mui/material/styles';
-import {useContext} from "react";
-import InputBase from '@mui/material/InputBase';
-import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import SearchIcon from '@mui/icons-material/Search';
-import { width } from "@mui/system";
+import { Outlet } from 'react-router-dom';
+import { styled } from '@mui/styles';
+import Navbar from './Navbar/Navbar';
 
-function TopBar (){
-    const theme = useTheme();
+const MainLayoutRoot = styled('div')(
+    ({ theme }) => ({
+        backgroundColor: theme.palette.background.default,
+        display: 'flex',
+        height: '100%',
+        overflow: 'hidden',
+        width: '100%'
+    })
+);
+
+const MainLayoutWrapper = styled('div')({
+    display: 'flex',
+    flex: '1 1 auto',
+    overflow: 'hidden',
+    paddingTop: 84
+});
+
+const MainLayoutContainer = styled('div')({
+    display: 'flex',
+    flex: '1 1 auto',
+    overflow: 'hidden'
+});
+
+const MainLayoutContent = styled('div')({
+    flex: '1 1 auto',
+    height: '100%',
+    overflow: 'auto'
+});
+
+const MainLayout = () => {
     return (
-        <Box sx={{
-            display:"flex",
-            justifyContent:"space-between",
-            p:2,
-            height:"40px",
-            backgroundColor:`${theme.palette.background.default}`,
-        }}>
+        <MainLayoutRoot>
+            <Navbar />
+            <MainLayoutWrapper>
+                <MainLayoutContainer>
+                    <MainLayoutContent>
+                        <Outlet />
+                    </MainLayoutContent>
+                </MainLayoutContainer>
+            </MainLayoutWrapper>
+        </MainLayoutRoot>
+    );
+};
 
-            {/* Search */}
-            <Box sx={{
-                display:'flex',
-                borderRadius:"3px"
-            }}>
-                <InputBase sx={{
-                    p: 1, 
-                    ml: 2,
-                    flex: 1,
-                    backgroundColor: theme.palette.background.paper,
-                    border: `2px solid ${theme.palette.primary.contrastText}`, 
-                    borderRadius: '50vh' 
-                    }} placeholder="Search" />
-                <IconButton>
-                <SearchIcon />
-                </IconButton>
-            </Box>
-
-            {/* Icons */}
-            <Box display="flex">
-                <IconButton sx={{ml: "auto"}}>
-                    <NotificationsOutlinedIcon />
-                </IconButton>
-                <IconButton>
-                    <SettingsOutlinedIcon />
-                </IconButton>
-                <IconButton>
-                    <PersonOutlineOutlinedIcon />
-                </IconButton>
-            </Box>
-        </Box>
-        )
-}
-
-export default TopBar;
+export default MainLayout;
