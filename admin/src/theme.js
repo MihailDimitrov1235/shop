663b0ee4477bf552230ef20e6c8e36e9a16b8a45
MihailDimitrov1235 <107298750+MihailDimitrov1235@users.noreply.github.com>
2022-12-05 18:54:36 +0200

    Update theme.js

--- admin/src/theme.js
+++ admin/src/theme.js
@@ -1,94 +1,70 @@
-import { createTheme } from '@mui/material';
-const { palette } = createTheme();
-const { augmentColor } = palette;
-const createColor = (mainColor) => augmentColor({ color: { main: mainColor } });
+import { useState } from "react";
+import React, { Component } from "react";
+import { Sidebar,useProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+//import 'react-pro-sidebar/dist/styles';
+import { Box, useTheme } from "@mui/system";
+import Typography from '@mui/material/Typography';
+import { Link } from "react-router-dom";
+import { IconButton } from "@mui/material";
+import ReactRoundedImage from "react-rounded-image";
 
-const bordoRedColor = createColor('#96011c');
+import MenuIcon from '@mui/icons-material/Menu';
 
-const theme = createTheme({
-  palette: {
-    bordoRed: bordoRedColor,
-    background: {
-      default: '#f4f6f8',
-      paper: '#ffffff',
-      adminMenu: '#4f5d73',
-      adminMenuDarker: '#465367',
-    },
-    primary: {
-      contrastText: '#96011c',
-      main: '#ffffff',
-      mainDarker: '#aaaaaa'
-    },
-    text: {
-      primary: '#172b4d',
-      secondary: '#6b778c',
-      white: '#ffffff',
-      black: '#000000'
+import DashboardIcon from '@mui/icons-material/Dashboard';
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+
+const SideBar = () =>{
+    const theme = useTheme();
+    const { collapseSidebar } = useProSidebar();
+    const hoverStyle = {
+        backgroundColor:`${theme.palette.background.paper}`,
+        color:`${theme.palette.primary.contrastText}`,
     }
-  },
-  shadows: [
-    'none',
-    '0 0 0 1px rgba(63,63,68,0.05), 0 1px 2px 0 rgba(63,63,68,0.15)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 2px 2px -2px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 3px 4px -2px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 3px 4px -2px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 4px 6px -2px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 4px 6px -2px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 4px 8px -2px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 5px 8px -2px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 6px 12px -4px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 7px 12px -4px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 6px 16px -4px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 7px 16px -4px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 8px 18px -8px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 9px 18px -8px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 10px 20px -8px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 11px 20px -8px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 12px 22px -8px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 13px 22px -8px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 14px 24px -8px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 16px 28px -8px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 18px 30px -8px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 20px 32px -8px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 22px 34px -8px rgba(0,0,0,0.25)',
-    '0 0 1px 0 rgba(0,0,0,0.31), 0 24px 36px -8px rgba(0,0,0,0.25)'
-  ],
-  typography: {
-    h1: {
-      fontWeight: 500,
-      fontSize: 35,
-      letterSpacing: '-0.24px'
-    },
-    h2: {
-      fontWeight: 500,
-      fontSize: 29,
-      letterSpacing: '-0.24px'
-    },
-    h3: {
-      fontWeight: 500,
-      fontSize: 24,
-      letterSpacing: '-0.06px'
-    },
-    h4: {
-      fontWeight: 500,
-      fontSize: 20,
-      letterSpacing: '-0.06px'
-    },
-    h5: {
-      fontWeight: 500,
-      fontSize: 16,
-      letterSpacing: '-0.05px',
-      display:'inline'
-    },
-    h6: {
-      fontWeight: 500,
-      fontSize: 14,
-      letterSpacing: '-0.05px'
-    },
-    overline: {
-      fontWeight: 500
-    }
-  }
-});
 
-export default theme;
+    return (
+        <Box sx={{
+            display:'flex',
+            height:'100%',
+        }}>
+            <Sidebar backgroundColor={theme.palette.background.paper} width="300px">
+                <Box height="auto" pl="15px" pr="auto" backgroundColor={theme.palette.primary.contrastText}>
+                    <IconButton onClick={() => collapseSidebar()}>
+                        <MenuIcon sx={{ color: theme.palette.primary.main }} />
+                    </IconButton>
+                </Box>
+                <Menu menuItemStyles={{
+                    button: ({ level, active, disabled }) => {
+                        // only apply styles on first level elements of the tree
+                        return {
+                            color: disabled ? `${theme.palette.primary.contrastText}` : `${theme.palette.primary.mainDarker}`,
+                            //backgroundColor: active ? '#eecef9' : undefined,
+                            backgroundColor: `${theme.palette.background.paper}`,
+                            ":hover":hoverStyle,
+                        };
+                    },
+                }}>
+                    <MenuItem active='true' disabled='false' icon=<DashboardIcon/> > Dashboard </MenuItem>
+                    <SubMenu label="Accounts" icon=<ManageAccountsIcon/> >
+                        <MenuItem> Users </MenuItem>
+                        <MenuItem> Admins </MenuItem> 
+                    </SubMenu>
+                    <SubMenu label="Projects" icon=<ShoppingBagIcon/> >
+                        <MenuItem> Add Project </MenuItem>
+                        <MenuItem> View Projects </MenuItem>
+                    </SubMenu>
+                    <MenuItem icon=<CalendarMonthIcon/> > Calendar </MenuItem>
+                </Menu>
+            </Sidebar>
+        </Box>
+    )
+}
+
+export default SideBar;
