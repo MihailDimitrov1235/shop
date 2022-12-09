861dbdc6bc962e0eff802f8d5ae3a2b492c7c7f3
Miroslav Balev <m.balev@editcreation.com>
2022-12-09 20:55:26 +0200

    filters comments

--- client/src/components/products/ProductFilters.js
+++ client/src/components/products/ProductFilters.js
@@ -2,6 +2,11 @@ import { Container } from '@mui/material';
 import FilterCard from '../filters/FilterCard';
 
 const ProductFilters = () => {
+    //nau4na oblast
+    //vip razrabotka - dezertaciq, monografiq, doklad, statiq, obzor
+    //ezik na razrabotkata
+    //klu4ovi dumi
+
     return (
         <Container>
             <FilterCard />
