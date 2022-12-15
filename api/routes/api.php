20bd340622fd3ce96d3a7859bd9b4d9df696a994
Misho <Mighty.strong1235@gmail.com>
2022-12-15 21:57:36 +0200

    Added payment to api

--- api/routes/api.php
+++ api/routes/api.php
@@ -3,21 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\PaymentController;
+
+Route::post('/payment', [PaymentController::class, 'pay'])->middleware('auth');
+
 Route::get('/project', [ProjectController::class, 'index']);
 Route::post('/project', [ProjectController::class, 'store'])->middleware('auth');
 Route::get('/project/{id}', [ProjectController::class, 'view']);
