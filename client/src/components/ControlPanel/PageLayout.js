e910fb0954ea3cf9ab57549d1d50835f67ebbeda
Miroslav Balev <m.balev@editcreation.com>
2022-12-28 23:48:34 +0200

    breadcrumbs in admin page

--- client/src/components/ControlPanel/PageLayout.js
+++ client/src/components/ControlPanel/PageLayout.js
@@ -1,29 +1,56 @@
-import { Box, IconButton, Typography, Container, Divider } from "@mui/material";
+import {
+    Box,
+    Typography,
+    Divider,
+    Breadcrumbs,
+    Link
+} from "@mui/material";
 import DownloadIcon from '@mui/icons-material/Download';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, Link as RouterLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { useTranslation } from 'react-i18next';
 
 const PageLayout = ({ title }) => {
     const { t } = useTranslation();
+    const location = useLocation();
+    const parts = location.pathname.split('/').slice(2);
 
     return (
         <Box sx={{
-            p:4,
+            p: 4,
         }}>
             {/* HEADER */}
             <Box>
                 <Box sx={{ pb: 2 }}>
-                    <Typography variant="h3">{t(title)}</Typography>
+                    <Breadcrumbs
+                        aria-label="breadcrumb"
+                        sx={{
+                            '.MuiBreadcrumbs-separator': {
+                                fontSize: 20
+                            }
+                        }}
+                    >
+                        {parts.map((el, index, parts) => {
+                            if (index + 1 === parts.length) {
+                                return <Typography color='text.black' variant='h4' key={index}>{t(el)}</Typography>
+                            } else {
+                                return (
+                                    <Link
+                                        component={RouterLink}
+                                        variant='h4'
+                                        underline='none'
+                                        color='text.black'
+                                        to=''
+                                        key={index}
+                                    >
+                                        {t(el)}
+                                    </Link>
+                                );
+                            }
+                        })}
+                    </Breadcrumbs>
                 </Box>
-                <Divider sx={{ mb: 5 }}/>
-                {/* <Box sx ={{
-                    ml:'auto',
-                }}>
-                    <IconButton>
-                        <DownloadIcon/>
-                    </IconButton>
-                </Box> */}
+                <Divider sx={{ mb: 5 }} />
             </Box>
             {/* CONTENT */}
             <Box height={"100%"} width={"100%"} ml={"auto"} mr={"auto"}>
