8bb994995b45e157266efa660e0f8abccb4bb604
StefanKozhuharov <107200328+StefanKozhuharov@users.noreply.github.com>
2022-12-29 11:59:51 +0200

    database migrations

---
+++ api/database/migrations/2022_12_29_094022_create_product_trans_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_trans', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("shortDescription");
+            $table->string("longDescription");
+            $table->foreignId('product_id')->constrained('products');
+            $table->string("lang");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_trans');
+    }
+};
