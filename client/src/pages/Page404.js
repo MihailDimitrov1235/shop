124a57b63cd80574964e824077c49e9b39f6a244
Misho <Mighty.strong1235@gmail.com>
2023-01-06 21:43:00 +0200

    added 404 page

---
+++ client/src/pages/Page404.js
@@ -0,0 +1,15 @@
+const Page404 = () =>{
+    return(
+        <div style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            height:"100%",
+            width:"100%",
+        }}>
+            <h1>Page not found</h1>
+        </div>
+    )
+}
+
+export default Page404;
