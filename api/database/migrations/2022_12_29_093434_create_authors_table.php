c9ccd21040ff91324d425cf343168efc924e7fca
Miroslav Balev <zevs8@abv.bg>
2023-01-11 13:45:55 +0200

    authors have one name field

--- api/database/migrations/2022_12_29_093434_create_authors_table.php
+++ api/database/migrations/2022_12_29_093434_create_authors_table.php
@@ -15,9 +15,7 @@ return new class extends Migration
     {
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
-            $table->string("firstName");
-            $table->string("middleName");
-            $table->string("lastName");
+            $table->string("name");
             $table->string("tel");
             $table->string("email");
             $table->timestamps();
