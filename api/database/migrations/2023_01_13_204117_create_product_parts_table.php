4dedd8fd83742e11dd4a738ea556fa8a48f88d95
Miroslav Balev <m.balev@editcreation.com>
2023-01-13 23:24:45 +0200

    more product tables

---
+++ api/database/migrations/2023_01_13_204117_create_product_parts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_parts', function (Blueprint $table) {
+            $table->id();
+            $table->double('price', 8, 2);
+            $table->foreignId('product_id')->constrained('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_parts');
+    }
+};
