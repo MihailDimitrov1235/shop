dcba87f553be0dcef625bb1580565f308e35d907
Misho <Mighty.strong1235@gmail.com>
2023-01-15 12:16:05 +0200

    Cart page design changes

--- client/src/pages/PublicWebsite/Cart/CartItem.js
+++ client/src/pages/PublicWebsite/Cart/CartItem.js
@@ -1,55 +1,14 @@
-import { Box, Button, Card, CardMedia, Grid, Typography } from '@mui/material';
+import { Box, IconButton, Card, CardMedia, Grid, Typography } from '@mui/material';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 
 
-const CartItem = ({ item }) => {
+const CartItem = ({ item, removeFromCart }) => {
+  console.log(item.id)
   return (
-      <Card style={{marginBottom:'20px', width:'100%'}}>
-        {/* <Box display = 'flex'
-        justifyContent = 'space-between'
-        fontFamily = 'Arial, Helvetica, sans-serif'
-        height='170px'
-        >
-        <h3>{item.title}</h3>
-        <div style={{flex: '1'}}>
-          <p>Price: ${item.price}</p>
-          {/* <p>Total: ${(item.amount * item.price).toFixed(2)}</p> */}
-        {/* </div>
-        <div style={{flex: '1'}}>
-          <Button
-            size="small"
-            disableElevation
-            variant="contained"
-            // onClick={() => removeFromCart(item.id)}
-            style={{
-                display: 'flex',
-                justifyContent: 'space-between'
-            }}
-          >
-            -
-          </Button>
-          <Button
-            size="small"
-            disableElevation
-            variant="contained"
-            // onClick={() => addToCart(item)}
-            style={{
-                display: 'flex',
-                justifyContent: 'space-between'
-            }}
-          >
-            +
-          </Button>
-        </div>
-        <CardMedia src={item.image} alt={item.title} component='img' style={{
-            width: 'auto',
-            height:'100%',
-            objectFit: 'cover',
-            marginLeft: '40px',
-        }} />
-        </Box> */}
+      <Card style={{marginTop:'20px', width:'100%'}}>
         <Grid container>
-            <Grid xs={6}>
+            <Grid xs={5}>
                 <Box display={'flex'}>
                     <CardMedia src={item.image} alt={item.title} component='img' style={{
                         width: 'auto',
@@ -70,6 +29,11 @@ const CartItem = ({ item }) => {
             </Grid>
             <Grid xs={2}>
                 <Typography variant='h6' style={{ textAlign: 'center'}}>Total</Typography>
+            </Grid>
+            <Grid xs={1} textAlign='right'>
+                <IconButton onClick={() => removeFromCart(item.id)}>
+                    <CancelIcon/>
+                </IconButton>
             </Grid>
         </Grid>
       </Card>
