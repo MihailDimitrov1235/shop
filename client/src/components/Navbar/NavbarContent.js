88c66097ce18b58f2da59c20fd53414a938a80db
Misho <Mighty.strong1235@gmail.com>
2023-02-27 19:42:03 +0200

    topbar animation changes

--- client/src/components/Navbar/NavbarContent.js
+++ client/src/components/Navbar/NavbarContent.js
@@ -3,20 +3,37 @@ import NavItem from './NavItem';
 import LanguageSwitcher from './LanguageSwitcher';
 import ProfileItem from './ProfileItem';
 import PropTypes from 'prop-types';
+import { useGesture } from '@use-gesture/react';
+import { useSpring, animated} from '@react-spring/web'
 
 const NavbarContent = ({ item }) => {
+
+    const [{transform}, api] = useSpring( () => ({
+        transform: "scale(1)",
+
+      }));
+
+    const bind = useGesture({
+        onHover: ({ hovering }) => api({transform:hovering? 'scale(1.1)': 'scale(1)'}),
+})
+
     return (
         <>
             {item.type === 'link' && (
+                
                 <NavItem
                     title={item.title}
                     href={item.href}
                     {...item}
                 />
+                
             )}
 
             {item.type === 'langSwitcher' && (
-                <LanguageSwitcher />
+                <animated.div {...bind()} style={{transform}}>
+                    <LanguageSwitcher />
+                </animated.div>
+                
             )}
 
             {item.type === 'divider' && (
