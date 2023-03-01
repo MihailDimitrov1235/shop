85ff3650681a2a57440c6f97b8c954ac5d21e803
Miroslav Balev <zevs8@abv.bg>
2023-03-01 21:33:34 +0200

    footer changes and responsive design

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import {
     Box,
+    Container,
     Typography
 } from '@mui/material';
 import topSvg from './Vector.svg';
@@ -10,66 +11,78 @@ const Footer = () => {
             <Box sx={{ display: 'flex' }}>
                 <img style={{ width: '100%' }} src={topSvg} alt="curve" />
             </Box>
-      
-            <Box width="100%" bgcolor={"#96011c"} display={'flex'} paddingBottom='40px'>
-                <Box width={'50%'} alignItems={'center'} justifyContent='center' color={'white'} textAlign='center'>
-                    <Box width={'300px'} marginLeft='auto' marginRight={'auto'}>
-                        <Typography variant='h3' paddingBottom={'50px'} display='block'>КОНТАКТИ</Typography>
-                        <Typography variant='p' display='block'>
-                            София, 1040, ул. „15 ноември“ №1
-                        </Typography>
-                        <Typography variant='p' display='block'>
-                            (+359 2) 979 53 33
-                        </Typography>
-                        <Typography variant='p' display='block' paddingBottom={'50px'}>
-                            (+359 2) 979 52 23
-                        </Typography>
-                        <Typography variant='p'>
-                            Администрация на БАН
-                            Връзки с обществеността
-                            Звена на БАН
-                        </Typography>
+
+            <Box sx={{ backgroundColor: 'background.bordoRed' }}>
+                <Container maxWidth={'false'} sx={{ width: '85%', margin: '0 auto' }}>
+                    <Box sx={{
+                            display: 'flex',
+                            justifyContent: 'space-between',
+                            alignItems: 'center',
+                            pb: '40px',
+                            flexDirection: { xs: 'column', md: 'row' },
+                            gap: 5
+                        }}
+                    >
+                        <Box sx={{ flexGrow: 1, color: 'white', textAlign: 'center' }}>
+                            <Box sx={{ width: '300px', margin: '0 auto' }}>
+                                <Typography variant='h3' paddingBottom={'50px'} display='block'>КОНТАКТИ</Typography>
+                                <Typography variant='p' display='block'>
+                                    София, 1040, ул. „15 ноември“ №1
+                                </Typography>
+                                <Typography variant='p' display='block'>
+                                    (+359 2) 979 53 33
+                                </Typography>
+                                <Typography variant='p' display='block' paddingBottom={'50px'}>
+                                    (+359 2) 979 52 23
+                                </Typography>
+                                <Typography variant='p'>
+                                    Администрация на БАН
+                                    Връзки с обществеността
+                                    Звена на БАН
+                                </Typography>
+                            </Box>
+                        </Box>
+
+                        <Box sx={{ flexGrow: 1, textAlign: 'center', color: 'white' }}>
+                            <Box sx={{ width: '400px', margin: '0 auto' }}>
+                                <Typography variant='h3' paddingBottom={'20px'} display='block'>КАРИЕРИ</Typography>
+                                <Typography variant='p' paddingBottom={'50px'} display='block'>
+                                    Обяви и конкурси по ЗРАСРБ
+                                    Конкурси за избор на директори на СЗ на БАН
+                                    Регистър на присъдените академични длъжности
+                                    Регистър на присъдените степени
+                                    Докторски програми
+                                </Typography>
+
+                                <Typography variant='h3' paddingBottom={'20px'} display='block'>ФИНАНСОВА И СТОПАНСКА ДЕЙНОСТ</Typography>
+                                <Typography variant='p' display={'block'}>
+                                    Бюджет и финансова информация
+                                </Typography>
+                                <Typography variant='p' display={'block'}>
+                                    Профил на купувача
+                                </Typography>
+                                <Typography variant='p' display={'block'}>
+                                    Имоти под наем
+                                </Typography>
+                                <Typography variant='p' display={'block'}>
+                                    Почивно дело
+                                </Typography>
+                            </Box>
+                        </Box>
                     </Box>
-                </Box>
 
-                <Box width={'50%'} alignItems={'center'} justifyContent='center' color={'white'} textAlign='center'>
-                    <Box width={'400px'} marginLeft='auto' marginRight={'auto'}>
-                        <Typography variant='h3' paddingBottom={'20px'} display='block'>КАРИЕРИ</Typography>
-                        <Typography variant='p' paddingBottom={'50px'} display='block'>
-                            Обяви и конкурси по ЗРАСРБ
-                            Конкурси за избор на директори на СЗ на БАН
-                            Регистър на присъдените академични длъжности
-                            Регистър на присъдените степени
-                            Докторски програми
-                        </Typography>
-
-                        <Typography variant='h3' paddingBottom={'20px'} display='block'>ФИНАНСОВА И СТОПАНСКА ДЕЙНОСТ</Typography>
-                        <Typography variant='p' display={'block'}>
-                            Бюджет и финансова информация
-                        </Typography>
-                        <Typography variant='p' display={'block'}>
-                            Профил на купувача
-                        </Typography>
-                        <Typography variant='p' display={'block'}>
-                            Имоти под наем
-                        </Typography>
-                        <Typography variant='p' display={'block'}>
-                            Почивно дело
-                        </Typography>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexDirection: { xs: 'column', md: 'row' } }}>
+                        <Box sx={{ flexGrow: 1, textAlign: { xs: 'center', md: 'left' } }}>
+                            <Typography color={'white'}>© 2023 Българска академия на науките | Някои права запазени | Политика за бисквитките</Typography>
+                        </Box>
+                        <Box sx={{ display: 'flex', justifyContent: 'space-evenly', p: '10px', width: '40%' }}>
+                            <img src="/static/images/icons/FacebookIcon.png" height={'30px'} />
+                            <img src="/static/images/icons/TwitterIcon.png" height={'30px'} />
+                            <img src="/static/images/icons/InstagramIcon.png" height={'30px'} />
+                            <img src="/static/images/icons/YoutubeIcon.png" height={'30px'} />
+                        </Box>
                     </Box>
-                </Box>
-            </Box>
-
-            <Box width="100%" bgcolor={"#96011c"} display={'flex'}>
-                <Box width={'70%'} display="flex" alignItems={'center'} justifyContent='center'>
-                    <Typography color={'white'}>© 2023 Българска академия на науките | Някои права запазени | Политика за бисквитките</Typography>
-                </Box>
-                <Box width={'30%'} display="flex" justifyContent={'space-evenly'} padding={'10px'}>
-                    <img src="/static/images/icons/FacebookIcon.png" height={'30px'} />
-                    <img src="/static/images/icons/TwitterIcon.png" height={'30px'} />
-                    <img src="/static/images/icons/InstagramIcon.png" height={'30px'} />
-                    <img src="/static/images/icons/YoutubeIcon.png" height={'30px'} />
-                </Box>
+                </Container>
             </Box>
         </footer>
     );
