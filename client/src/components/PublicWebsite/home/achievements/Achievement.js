1cba3fa42b2120e0dea26f4e28e8ea2c4532cbc1
Miroslav Balev <zevs8@abv.bg>
2023-03-01 20:58:32 +0200

    achievement card design

--- client/src/components/PublicWebsite/home/achievements/Achievement.js
+++ client/src/components/PublicWebsite/home/achievements/Achievement.js
@@ -3,41 +3,47 @@ import { Box, Typography, Button, CardMedia, Card, duration } from '@mui/materia
 import "./AchievementCss.css"
 import { animated, useSpring } from '@react-spring/web';
 import { useInView } from 'react-intersection-observer';
-function Achievement( props ){
+function Achievement(props) {
+    const { icon: Icon } = props;
 
-    const { ref, inView} = useInView({
+    const { ref, inView } = useInView({
         /* Optional options */
         triggerOnce: true,
         threshold: 0.5,
-      });
+    });
 
     const styles = useSpring({
-        opacity: inView? 1 : 0,
-        y: inView? 0 : 40,
-        config:{duration:500}
-      })
-    return(
+        opacity: inView ? 1 : 0,
+        y: inView ? 0 : 40,
+        config: { duration: 500 }
+    })
+    return (
         <animated.div style={styles} ref={ref}>
-        <Box sx={{ textAlign: 'center' }}>
-
-            <Typography className='topText' 
-            marginLeft='auto' 
-            marginRight={'auto'}
-            width={'100px'}>
-                {props.name}
-            </Typography>
-            <Card className='card'>
-                <Box width={'70%'} paddingLeft={'40px'} paddingRight='40px'>
-                    {props.desc}
-                </Box>
-                <Box width={'30%'} height='100%' paddingRight='40px'>
-                    <props.icon sx={{
-                        height:'100%',
-                        width:'100%'
-                    }}/>
-                </Box>
-            </Card>
-        </Box>
+            <Box sx={{ textAlign: 'center' }}>
+                <fieldset className='achievement'>
+                    <legend>
+                        <Typography className='topText'
+                            marginLeft='auto'
+                            marginRight={'auto'}
+                            width={'100px'}>
+                            {props.name}
+                        </Typography>
+                    </legend>
+                    <Box className='achievement-content'>
+                        <Box width={'70%'} paddingLeft={'40px'} paddingRight='40px'>
+                            {props.desc}
+                        </Box>
+                        <Box width={'30%'} height='100%' paddingRight='40px'>
+                            <Icon sx={{
+                                    height: '100%',
+                                    width: '100%',
+                                    color: 'text.secondary'
+                                }}
+                            />
+                        </Box>
+                    </Box>
+                </fieldset>
+            </Box>
         </animated.div>
     )
 }
