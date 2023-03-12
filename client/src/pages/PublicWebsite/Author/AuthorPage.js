4ad6b75ea3214124d094d71a3f170460cc1ace3f
Misho <Mighty.strong1235@gmail.com>
2023-03-12 21:31:15 +0200

    author page

--- client/src/pages/PublicWebsite/Author/AuthorPage.js
+++ client/src/pages/PublicWebsite/Author/AuthorPage.js
@@ -1,3 +1,10 @@
+import {Container, Box, Typography} from '@mui/material';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import LinkIcon from '@mui/icons-material/Link';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import { display } from '@mui/system';
+
 
 const props = {
     name:'Mihail Dimitrov',
@@ -6,8 +13,8 @@ const props = {
     email:'mighty.strong1235@gmail.com',
     links:[
         'facebook.com/misho',
-        'twitter.com/misho',
-        'linkedin.com/misho'
+        'twitter.com/GothamChess',
+        'linkedin.com/in/mihail-d/',
     ],
     description:"Lorem ipsum dolor sfiuwegtf qw79egfqgw ew67o 8o7wqg8o7 ftwg8oe 7gf8ow7qeg f67owetgqf67 qit amet, consectetur adipiscing elit. Ut id purus ante. Ut vena, euismod et ante vel, consectetur accumsan diam. Aenean iaculis posuere odio, sit amet pulvinar mauris convallis non. Curabitur tempor ultrices eros, mattis mollis sapien pharetra vel. Incongue vulputate. Nam non diam pellentesque, lacinia ex eget, tristique sem.",
     products:[
@@ -19,7 +26,60 @@ const props = {
 }
 
 export default function AuthorPage(){
+
+    console.log(props.links)
+
+    const facebookRegex = /(?:https?:\/\/)?(?:www\.)?(mbasic.facebook|m\.facebook|facebook|fb)\.(com|me)\/(?:(?:\w\.)*#!\/)?(?:pages\/)?(?:[\w\-\.]*\/)*([\w\-\.]*)/;
+    const linkedinRegex = /^(http(s)?:\/\/)?([\w]+\.)?linkedin\.com\/(pub|in|profile)\/([-a-zA-Z0-9]+)\/*/gm;
+    const twitterRegex = /((?:https?:\/\/)?twitter.com\/(?![a-zA-Z0-9_]+\/)([a-zA-Z0-9_]+))/;
+
+
     return(
-        <></>
+        <Box >
+            <Box width={'100%'} height='30%' overflow={'hidden'} sx={{
+                display:'flex',
+                justifyContent:'center',
+                alignContent:'center'
+            }}>
+                <img width={'100%'} src='https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'/>
+            </Box>
+            <Box className='topSection' width='100%'>
+                <Container className='topContent' style={{
+                    backgroundColor:'white',
+                    display:'flex',
+                }}>
+                    <Box boxShadow={'7px 7px 10px'} marginRight='50px' position={'relative'} bottom={'100px'} className='authorImg' overflow={'hidden'} flex='1.5' border={'solid white 10px'} borderRadius='50%' style={{
+                        aspectRatio:'1/1',
+                        display:'flex',
+                        justifyContent:'center',
+                        alignContent:'center'
+                    }}>
+                        <img  height='100%' src='https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80' />
+                    </Box>
+                    <Box display='flex' justifyContent={'center'} flex={'4'} flexDirection={'column'} >
+                        <Typography variant='h1' marginBottom={'20px'} textAlign='center'>{props.name}</Typography>
+                        <Typography variant='subtitle1' marginBottom={'30px'} textAlign='center' >{props.ocupation}</Typography>
+                        <Box className='authorLinks' display={'flex'} flexDirection='row' justifyContent={'space-evenly'} flexWrap="wrap" >
+                        {props.links.map(link =>(
+                            <Box display={'flex'} marginBottom='10px'>
+                                {facebookRegex.exec(link)? <FacebookIcon/>
+                                    : linkedinRegex.exec(link)? <LinkedInIcon/>
+                                        : twitterRegex.exec(link)? <TwitterIcon/>
+                                            : <LinkIcon/>}
+                                <Typography>
+                                    {link}
+                                </Typography>
+                            </Box>
+                        ))}
+                        </Box>
+                    </Box>
+                </Container>
+            </Box>
+            <Container className='mainContent' width='100%'>
+
+            </Container>
+            
+        </Box>
+
     );
 }
