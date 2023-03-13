e137029350ece4491b6d4adb7a7c631e6ad4c7c5
Misho <Mighty.strong1235@gmail.com>
2023-03-13 21:27:19 +0200

    author page changes

--- client/src/pages/PublicWebsite/Author/AuthorPage.js
+++ client/src/pages/PublicWebsite/Author/AuthorPage.js
@@ -1,14 +1,16 @@
-import {Container, Box, Typography, Button} from '@mui/material';
+import {Container, Box, Typography, Card} from '@mui/material';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import LinkIcon from '@mui/icons-material/Link';
 import TwitterIcon from '@mui/icons-material/Twitter';
+import PhoneIcon from '@mui/icons-material/Phone';
+import EmailIcon from '@mui/icons-material/Email';
 
 
 const props = {
     name:'Mihail Dimitrov',
     ocupation:'Student in the national highschool of sciences',
-    phones:'1234567890',
+    phone:'1234567890',
     email:'mighty.strong1235@gmail.com',
     links:[
         'facebook.com/misho',
@@ -45,39 +47,74 @@ export default function AuthorPage(){
                 <img width={'100%'} src='https://images.unsplash.com/photo-1519681393784-d120267933ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8&w=1000&q=80'/>
             </Box>
             <Box className='topSection' width='100%'>
-                <Container className='topContent' style={{
-                    backgroundColor:'white',
-                    display:'flex',
-                }}>
-                    <Box boxShadow={'7px 7px 10px'} marginRight='50px' position={'relative'} bottom={'100px'} className='authorImg' overflow={'hidden'} flex='1.5' border={'solid white 10px'} borderRadius='50%' style={{
-                        aspectRatio:'1/1',
+                <Container className='topContent'>
+                    <Card sx={{
                         display:'flex',
-                        justifyContent:'center',
-                        alignContent:'center'
+                        overflow:'visible',
+                        px:'30px'
                     }}>
-                        <img  height='100%' src='https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80' />
-                    </Box>
-                    <Box display='flex' justifyContent={'center'} flex={'4'} flexDirection={'column'} >
-                        <Typography variant='h1' marginBottom={'20px'} textAlign='center'>{props.name}</Typography>
-                        <Typography variant='subtitle1' marginBottom={'30px'} textAlign='center' >{props.ocupation}</Typography>
-                        <Box className='authorLinks' display={'flex'} flexDirection='row' justifyContent={'space-evenly'} flexWrap="wrap" >
-                        {props.links.map(link =>(
-                                <Box display={'flex'} marginBottom='10px'>
-                                        {facebookRegex.exec(link)? <FacebookIcon/>
-                                            : linkedinRegex.exec(link)? <LinkedInIcon/>
-                                                : twitterRegex.exec(link)? <TwitterIcon/>
-                                                    : <LinkIcon/>}
-                                        <Typography>
-                                            {link}
-                                        </Typography>
-                                </Box>
-                        ))}
+                        <Box boxShadow={'7px 7px 10px'} marginRight='50px' position={'relative'} bottom={'100px'} className='authorImg' overflow={'hidden'} flex='1.5' border={'solid white 10px'} borderRadius='50%' style={{
+                            aspectRatio:'1/1',
+                            display:'flex',
+                            justifyContent:'center',
+                            alignContent:'center'
+                        }}>
+                            <img  height='100%' src='https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80' />
                         </Box>
-                    </Box>
+                        <Box display='flex' justifyContent={'center'} flex={'4'} flexDirection={'column'} >
+                            <Typography variant='h1' marginBottom={'20px'} textAlign='center'>{props.name}</Typography>
+                            <Typography variant='subtitle1' marginBottom={'30px'} textAlign='center' >{props.ocupation}</Typography>
+                            <Box className='authorLinks' display={'flex'} flexDirection='row' justifyContent={'space-evenly'} flexWrap="wrap" >
+                            {props.links.map(link =>(
+                                    <Box display={'flex'} marginBottom='10px'>
+                                            {facebookRegex.exec(link)? <FacebookIcon/>
+                                                : linkedinRegex.exec(link)? <LinkedInIcon/>
+                                                    : twitterRegex.exec(link)? <TwitterIcon/>
+                                                        : <LinkIcon/>}
+                                            <Typography>
+                                                {link}
+                                            </Typography>
+                                    </Box>
+                            ))}
+                            </Box>
+                        </Box>
+                    </Card>
                 </Container>
             </Box>
-            <Container className='mainContent' width='100%'>
 
+            <Container className='mainContent' sx={{
+                display:'flex',
+                width:'100%',
+                flexDirection:'row',
+            }}>
+                <Card className='authorSideBar' sx={{
+                    display:'flex',
+                    flexDirection:'column',
+                    flex:'1',
+                    py:'20px',
+                    px:'5px',
+                    m:'10px',
+                    ml:'0',
+                }}>
+                    <Box display={'flex'} margin={'0 auto'}>
+                        <PhoneIcon/>
+                        <Typography variant='subtitle1'>{props.phone}</Typography>
+                    </Box>
+                    <Box display={'flex'} margin={'0 auto'}>
+                        <EmailIcon/>
+                        <Typography variant='subtitle1'>{props.email}</Typography>
+                    </Box>
+                </Card>
+                <Card className='authorContent' sx={{
+                    display:'flex',
+                    flex:'2',
+                    py:'20px',
+                    px:'5px',
+                    m:'10px',
+                    mr:'0',
+                }}>
+                    <Box></Box>
+                </Card>
             </Container>
             
         </Box>
