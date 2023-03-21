f15f5de19a5f1faea89a3d4e7454b0a24bd5f287
Misho <Mighty.strong1235@gmail.com>
2023-03-21 19:53:07 +0200

    cart responsive design

--- client/src/pages/PublicWebsite/Cart/CartItem.js
+++ client/src/pages/PublicWebsite/Cart/CartItem.js
@@ -6,46 +6,57 @@ import CancelIcon from '@mui/icons-material/Cancel';
 const CartItem = ({ item, removeFromCart }) => {
   return (
       <Card style={{marginTop:'20px', width:'100%'}}>
-        <Grid container>
-            <Grid item xs={5}>
-                <Box display={'flex'}>
+        <Grid container sx={{
+            position:'relative'
+        }}>
+            <Grid item md={6} xs={12}>
+                <Box display={'flex'} width='100%'>
                     <CardMedia src={item.image} alt={item.title} component='img' style={{
                         width: 'auto',
                         height:'150px',
                         objectFit: 'cover',
                     }} /> 
-                <Box sx={{
-                    display:'flex',
-                    flexDirection:'column',
-                    justifyContent:'center'
-                }}>
-                    <Typography variant='h3' >{item.title}</Typography>
-                    <Typography variant='h6' >{item.description}</Typography>
-                </Box>
+                    <Box sx={{
+                        width:'100%',
+                        px:1,
+                        display:'flex',
+                        flexDirection:'column',
+                        justifyContent:'center',
+                        textAlign:{md:'left',xs:'center'}
+                    }}>
+                        <Typography variant='h3' >{item.title}</Typography>
+                        <Typography variant='h6' >{item.description}</Typography>
+                    </Box>
                 </Box>
             </Grid>
-            <Grid item xs={2} sx={{
+            <Grid item md={2} xs={4} sx={{
                 display:'flex',
                 flexDirection:'column',
-                justifyContent:'center'
+                justifyContent:'center',
+                py:{md:0,xs:1},
             }}>
                 <Typography variant='h6' style={{ textAlign: 'center', justifyContent:'center'}}>Price</Typography>
             </Grid>
-            <Grid item xs={2} sx={{
+            <Grid item md={2} xs={4} sx={{
                 display:'flex',
                 flexDirection:'column',
-                justifyContent:'center'
+                justifyContent:'center',
+                py:{md:0,xs:1},
             }}>
                 <Typography variant='h6' style={{ textAlign: 'center'}}>Parts</Typography>
             </Grid>
-            <Grid item xs={2} sx={{
+            <Grid item md={2} xs={4} sx={{
                 display:'flex',
                 flexDirection:'column',
-                justifyContent:'center'
+                justifyContent:'center',
+                py:{md:0,xs:1},
             }}>
                 <Typography variant='h6' style={{ textAlign: 'center'}}>Total</Typography>
             </Grid>
-            <Grid item xs={1} textAlign='right'>
+            <Grid item textAlign='right' sx={{
+                position:'absolute',
+                right:'0'
+            }}>
                 <IconButton onClick={() => removeFromCart(item.id)}>
                     <CancelIcon/>
                 </IconButton>
