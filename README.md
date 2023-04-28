17a1c4fa3485bb33a0dbbfb494aa0f43f0931c12
MihailDimitrov1235 <mighty.strong1235@gmail.com>
2023-04-28 18:20:22 +0300

    request page changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # This is a webstore created on React and Laravel for the Bulgarian Academy of Sciences.
+
+## to-do
+### author and product requests databases
+
+## bugs
+### main page english
--- client/src/pages/ControlPanel/Requests/RequestAuthor.js
+++ client/src/pages/ControlPanel/Requests/RequestAuthor.js
@@ -1,4 +1,24 @@
+import { useTranslation } from 'react-i18next';
+
 const RequestAuthor = () =>{
+    const { t, i18n } = useTranslation();
+
+    const data = [
+        {id: 1, name:"Mihail", timePassedInHours: 100},
+        {id: 1, name:"Mihail", timePassedInHours: 100},
+        {id: 1, name:"Mihail", timePassedInHours: 100},
+        {id: 1, name:"Mihail", timePassedInHours: 100},
+        {id: 1, name:"Mihail", timePassedInHours: 100},
+        {id: 1, name:"Mihail", timePassedInHours: 100},
+    ]
+    
+    const headings = [
+        { id: 'id', label: t('author-id'), order: true },
+        { id: 'name', label: t('name'), order: true },
+        { id: 'email', label: t('email'), order: true },
+        { id: 'phone', label: t('phone'), order: false},
+    ];
+
     return(
     <>
 
