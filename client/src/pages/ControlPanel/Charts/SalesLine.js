41ae26a1545453eb7e60b53f7fa0d1d6f58ee084
MihailDimitrov1235 <mighty.strong1235@gmail.com>
2023-05-06 22:45:36 +0300

    test

--- client/src/pages/ControlPanel/Charts/SalesLine.js
+++ client/src/pages/ControlPanel/Charts/SalesLine.js
@@ -22,7 +22,12 @@ function SalesLine() {
   return (
     <ResponsiveLine
       data={data}
-      margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+      margin={{ 
+        top: 50, 
+        right: 110, 
+        bottom: 50, 
+        left: 60 
+      }}
       xScale={{ type: "point" }}
       yScale={{
         type: "linear",
