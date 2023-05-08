89cfbcc22b011742bb533b53df6f2bbd5aa1904e
Miroslav Balev <m.balev@editcreation.com>
2023-05-08 20:45:29 +0300

    posts migration, model and controller

---
+++ api/app/Http/Controllers/PostController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function store(Request $request)
+    {
+
+    }
+
+    public function edit(Request $request, $id)
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+    public function show()
+    {
+        
+    }
+}
---
+++ api/database/migrations/2023_05_08_174156_create_posts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('slug');
+            $table->string('title');
+            $table->longText('description');
+            $table->string('image_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+};
