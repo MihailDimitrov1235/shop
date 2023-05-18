b37bd72bc7d293a55675ae86d1397dfa0e9447b2
MihailDimitrov1235 <mighty.strong1235@gmail.com>
2023-05-18 19:12:06 +0300

    blogger migrations, model and controller

---
+++ api/app/Http/Controllers/BloggerController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\{
+    Blogger,
+    BloggerTrans
+};
+
+class BloggerController extends Controller
+{
+    // public function index(){}
+    public function getById($id){
+        
+    }
+}
---
+++ api/app/Models/Blogger.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blogger extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'phone',
+        'email',
+        'links',
+        'image_path'
+    ];
+
+    public function trans() {
+        return $this->hasMany(BloggerTrans::class);
+    }
+}
