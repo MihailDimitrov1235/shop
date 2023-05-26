6d086291ba9c54e7aa03066cdc0886aa0e78b2a7
Stefan Kozhuharov <stef.kozhuharov@gmail.com>
2023-05-26 12:56:27 +0300

    comment report table

---
+++ api/database/migrations/2023_05_26_095439_create_comment_reports_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comment_reports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('comment_id')->constrained('comments')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');//id of the reporter
+            $table->text('reason');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comment_reports');
+    }
+};
